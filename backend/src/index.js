ad4c633535e88a57b826a307d78708a6cb6f90ad
pedroimpulcetto <pedro.impulcetto@gmail.com>
2020-01-20 21:07:25 -0300

    Web Feito

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0-43yok.mongodb.net/devmaps?r
 	useCreateIndex: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
